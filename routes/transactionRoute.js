91854babfadc53a352e052ae13b895f3495ba12e
Asensio0227 <mtheesikhosana@gmail.com>
2024-12-08 12:28:28 +0200

    rt commit

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -22,7 +22,7 @@ router
   );
 router.route('/deposit').post(depositTransactions);
 router.route('/withdraw').post(withdrawalTransactions);
-router.route('/transfer').post(transferTransactions);
+router.route('/transfer/:id').post(transferTransactions);
 
 router.route('/statement/:accountNumber').post(retrieveBankStatement);
 router.route('/retrieve/:id').get(retrieveSingleTransactions);
